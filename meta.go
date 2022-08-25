3ebe744e7bfcc710e355942da29372b277a24ee4
zhangjinlong <zhangjinlong@xiaoyouzi.com>
2022-08-25 22:49:57 +0800

    code optimization

--- meta.go
+++ meta.go
@@ -2,10 +2,20 @@ package iface
 
 import (
 	_ "embed"
+	"github.com/gomelon/meta"
 )
 
 //go:embed iface.tmpl
 var TmplIface string
+
+func DefaultPkgGenFactory() meta.PkgGenFactory {
+	pkgParser := meta.NewPkgParser()
+	return meta.NewTmplPkgGenFactory(TmplIface,
+		meta.WithOutputFilename("iface"),
+		meta.WithPkgParser(pkgParser),
+		meta.WithMetaParser(meta.NewParser(pkgParser)),
+	)
+}
 
 const (
 	MetaIface   = "+iface.Iface"
@@ -26,3 +36,5 @@ type Iface struct {
 type Comment struct {
 	Value []string
 }
+
+//TODO 缺少Factory
